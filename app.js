960ad86d54d02d632811822ccc0ff9b9ee652895
Lily Jiang <ljiang@olin.edu>
2022-11-19 01:13:48 -0500

    add baseline code

---
+++ app.js
@@ -0,0 +1,22 @@
+// manages activity
+
+'use strict';
+
+// create reference to button in page
+const switcher = document.querySelector('.btn');
+
+
+// handle event for button click
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
