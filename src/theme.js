84737ca8e8ca62401335fdca091159ad2da64fd2
ljiang09 <ljiang@olin.edu>
2023-08-01 07:55:48 -0400

    add intro section

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#e85a4f",
+    },
+    secondary: {
+      main: "#e98074",
+    },
+    background: {
+      tan: "#eae7dc",
+    },
+    text: {
+      primary: "#8e8d8a",
+      secondary: "#ffffff",
+    },
+  },
+});
+
+export default theme;
