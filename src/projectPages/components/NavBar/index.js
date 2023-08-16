b74dc71479d045e365c6b9985167583209ce1874
ljiang09 <ljiang@olin.edu>
2023-08-16 19:52:00 -0400

    feat: add nav bar to all project pages

--- src/projectPages/components/NavBar/index.js
+++ src/projectPages/components/NavBar/index.js
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import { Button, Menu, MenuItem } from "@material-ui/core";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { useNavigate } from "react-router-dom";
+import { projects } from "../../../components/Projects/projects_data.js";
+
+import useStyles from "./styles.js";
+
 const NavBar = () => {
-  return <div>This is the nav bar</div>;
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+  const handleProjectsOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleProjectsClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProjectsClick = (pageUrl) => {
+    navigate(`/subpages/${pageUrl}`);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Button onClick={handleHomeClick} className={classes.homeBtn}>
+        Lily Jiang
+      </Button>
+      <Button onClick={handleProjectsOpen} className={classes.projectsBtn}>
+        Projects
+        {/* TODO: this should change to up icon when menu is open */}
+        <KeyboardArrowDownIcon className={classes.icon} />
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleProjectsClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        transformOrigin={{ vertical: "top", horizontal: "center" }}
+        getContentAnchorEl={null}
+        className={classes.menu}
+      >
+        {projects.map((project) => (
+          <MenuItem onClick={() => handleProjectsClick(project.pageUrl)}>
+            {project.name}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
 };
 
 export default NavBar;
---
+++ src/projectPages/components/NavBar/styles.js
@@ -0,0 +1,35 @@
+import { makeStyles } from "@material-ui/core/styles";
+import theme from "../../../theme.js";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: "100px",
+    background: theme.palette.primary.main,
+  },
+  homeBtn: {
+    fontSize: "30px",
+    color: "white",
+    textTransform: "none",
+    padding: "10px 20px",
+    margin: "10px 20px",
+  },
+  projectsBtn: {
+    fontSize: "24px",
+    color: "white",
+    textTransform: "none",
+    padding: "10px 20px",
+    margin: "10px 20px",
+  },
+  icon: {
+    marginLeft: "5px",
+    fontSize: "30px",
+  },
+  menu: {
+    maxHeight: "300px",
+  },
+}));
+
+export default useStyles;
