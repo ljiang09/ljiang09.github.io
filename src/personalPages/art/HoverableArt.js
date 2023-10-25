5d862fe690a98c22f6cbf1da263be38796ca9212
ljiang09 <ljiang@olin.edu>
2023-10-25 14:58:19 -0400

    remove unneeded import

--- src/personalPages/art/HoverableArt.js
+++ src/personalPages/art/HoverableArt.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
-import theme from "../../theme.js";
 
 const useStyles = makeStyles(() => ({
   root: {
