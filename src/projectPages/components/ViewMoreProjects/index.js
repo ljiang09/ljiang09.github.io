7cf7d6d16c187f2a1625deee51e4f0247fa54835
ljiang09 <ljiang@olin.edu>
2023-10-24 20:19:08 -0400

    add "view more projects" section

--- src/projectPages/components/ViewMoreProjects/index.js
+++ src/projectPages/components/ViewMoreProjects/index.js
@@ -1,19 +1,57 @@
-import { Typography } from "@material-ui/core";
+import { Typography, Grid, Button } from "@material-ui/core";
+import { NavLink, useParams } from "react-router-dom";
 import useStyles from "./styles.js";
-import { TAGS } from "../../../components/Projects/projects_data.js";
+import {
+  TAGS,
+  projects as project_data,
+} from "../../../components/Projects/projects_data.js";
 
 const ViewMoreProjects = ({ type }) => {
   const classes = useStyles();
 
+  const { subpage } = useParams();
+
+  const getProjects = (projects, substitution = null) => {
+    let updatedProjects = [];
+    if (projects.includes(subpage)) {
+      updatedProjects = projects.filter((project) => project !== subpage);
+      if (substitution) updatedProjects.push(substitution);
+    }
+
+    updatedProjects = updatedProjects.map((project) =>
+      project_data.find((projectObj) => projectObj.pageUrl === project)
+    );
+
+    return updatedProjects;
+  };
+
+  let displayedProjects = [];
+
   switch (type) {
     case TAGS.SOFTWARE:
+      displayedProjects = getProjects(
+        ["desktopPet", "keylogger", "sebelaApp"],
+        "makeupGuideApp"
+      );
+      break;
     case TAGS.IOS:
+      displayedProjects = getProjects(["sebelaApp", "makeupGuideApp"]);
       break;
     case TAGS.ROBOTICS:
+      displayedProjects = getProjects([
+        "panTiltScanner",
+        "lineFollowerRobot",
+        "https://supah32.github.io/pie-2022-03/",
+      ]);
       break;
     case TAGS.MECHANICAL:
+      displayedProjects = getProjects(["towLab"]);
       break;
     case TAGS.COURSE_PROJECTS:
+      displayedProjects = getProjects(
+        ["keylogger", "lineFollowerRobot", "rushHourGame"],
+        "desktopPet"
+      );
       break;
     default:
       break;
@@ -24,6 +62,46 @@ const ViewMoreProjects = ({ type }) => {
       <Typography className={classes.header}>
         View Other {type} Projects
       </Typography>
+
+      <Grid container className={classes.projects}>
+        {displayedProjects.map((project) => (
+          <Grid item>
+            {project.content ? (
+              <NavLink to={`/subpages/${project.pageUrl}`}>
+                <Button className={classes.link}>
+                  <div>
+                    <div className={classes.projectImage}>
+                      <img src={project.imgUrl} alt={project.name} />
+                    </div>
+                    <Typography className={classes.projectName}>
+                      {project.name}
+                    </Typography>
+                    <Typography className={classes.projectDate}>
+                      {project.date}
+                    </Typography>
+                  </div>
+                </Button>
+              </NavLink>
+            ) : (
+              <a href={project.pageUrl}>
+                <Button className={classes.link}>
+                  <div>
+                    <div className={classes.projectImage}>
+                      <img src={project.imgUrl} alt={project.name} />
+                    </div>
+                    <Typography className={classes.projectName}>
+                      {project.name}
+                    </Typography>
+                    <Typography className={classes.projectDate}>
+                      {project.date}
+                    </Typography>
+                  </div>
+                </Button>
+              </a>
+            )}
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
--- src/projectPages/components/ViewMoreProjects/styles.js
+++ src/projectPages/components/ViewMoreProjects/styles.js
@@ -13,6 +13,58 @@ const useStyles = makeStyles(() => ({
     fontSize: "30px",
     fontWeight: "bold",
   },
+  projects: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "50px",
+    marginTop: "50px",
+    // TODO: change this into a responsive sizing. breakpoints
+    "& .MuiGrid-item": {
+      borderRadius: "20px",
+      "& a": {
+        textDecoration: "none",
+      },
+    },
+  },
+  link: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+    background: "white",
+    borderRadius: "20px",
+    boxShadow: "0 0 20px " + theme.palette.text.primary,
+    "&:hover": {
+      background: theme.palette.background.gray,
+    },
+    "&:active": {
+      boxShadow: "0 0 10px " + theme.palette.text.primary,
+    },
+  },
+  projectImage: {
+    width: "275px",
+    height: "275px",
+    margin: "25px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    "& img": {
+      maxWidth: "100%",
+      minWidth: "100%",
+      width: "auto",
+      height: "auto",
+    },
+  },
+  projectName: {
+    fontSize: "30px",
+    textTransform: "none",
+  },
+  projectDate: {
+    fontSize: "20px",
+    textTransform: "none",
+    marginBottom: "25px",
+  },
 }));
 
 export default useStyles;
