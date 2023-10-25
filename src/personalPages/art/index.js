5303df1f944697984ca4b5a34070ec997c8face9
ljiang09 <ljiang@olin.edu>
2023-10-25 02:05:26 -0400

    add art

--- src/personalPages/art/index.js
+++ src/personalPages/art/index.js
@@ -1,8 +1,69 @@
+import NavBar from "../../components/NavBar";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import theme from "../../theme.js";
+import {
+  BookstoreDrawing,
+  BostonBuilding,
+  JapaneseCastle,
+  Kyoto,
+  NMNF,
+  RamenShopColored,
+  RamenShopUncolored,
+  RedRoof,
+} from "./media/index.js";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+    paddingTop: "60px",
+    margin: 0,
+    background: theme.palette.background.tan,
+    paddingBottom: "100px",
+    minHeight: "100vh",
+  },
+  title: {
+    fontSize: "40px",
+    fontWeight: "bold",
+  },
+  images: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: "30px",
+    width: "90vw",
+    "& img": {
+      border: "1px solid black",
+      boxShadow: "0px 0px 10px gray",
+      height: "70vh",
+    },
+  },
+}));
+
 const ArtHomePage = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Art Home Page</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className={classes.root}>
+        <Typography className={classes.title}>Art</Typography>
+        <div className={classes.images}>
+          <img src={BookstoreDrawing} alt="Bookstore Drawing" />
+          <img src={NMNF} alt="Six of Crows" />
+          {/* TODO: make uncolored appear on hover */}
+          <img src={RamenShopUncolored} alt="Ramen Shop Uncolored" />
+          <img src={RamenShopColored} alt="Ramen Shop Colored" />
+          <img src={BostonBuilding} alt="Boston Building" />
+          <img src={JapaneseCastle} alt="Japanese Castle" />
+          <img src={Kyoto} alt="Kyoto" />
+          <img src={RedRoof} alt="Red Roof" />
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/personalPages/art/media/index.js
@@ -0,0 +1,19 @@
+import BookstoreDrawing from "./Bookstore.jpg";
+import BostonBuilding from "./BostonBuilding.jpg";
+import JapaneseCastle from "./JapaneseCastle.jpg";
+import Kyoto from "./Kyoto.jpg";
+import NMNF from "./NMNF.jpg";
+import RamenShopColored from "./RamenShopColored.jpg";
+import RamenShopUncolored from "./RamenShopUncolored.jpg";
+import RedRoof from "./RedRoof.jpg";
+
+export {
+  BookstoreDrawing,
+  BostonBuilding,
+  JapaneseCastle,
+  Kyoto,
+  NMNF,
+  RamenShopColored,
+  RamenShopUncolored,
+  RedRoof,
+};
