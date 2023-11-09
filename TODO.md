ca846c6bc768524810769c407407c5d958a1d14a
ljiang09 <ljiang@olin.edu>
2023-11-09 10:53:20 -0500

    add fade between art images on hover

--- TODO.md
+++ TODO.md
@@ -1,4 +1,15 @@
 - figure out how to force starting at top of each page
 - add architecture blogs home page
-- add art to portfolio
 - finish "contact/about me" section
+- update resume link
+- make art images fade on hover
+
+Cool animation ideas:
+
+- videos that play only on hover. perhaps for projects? Inspo:
+  https://pisontechnology.com/index
+- project cards grow slightly bigger on hover. inspo:
+  https://pisontechnology.com/index
+
+- https://attach.fly.dev
+- scroll animation of tech stack/skills icons https://zackaryjamessantana.com
--- src/personalPages/art/HoverableArt.js
+++ src/personalPages/art/HoverableArt.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
+import CrossfadeImage from "react-crossfade-image";
 
 const useStyles = makeStyles(() => ({
   root: {
     boxShadow: ({ hovered }) =>
       hovered ? "0px 0px 5px gray" : "0px 0px 10px gray",
     padding: 0,
+    transition: "all 0.25s ease",
   },
 }));
 
 const HoverableArt = ({ src, hoverSrc = null, alt }) => {
   const [hovered, setHovered] = useState(false);
-  const classes = useStyles({ hovered });
+  const classes = useStyles({ hovered, hoverSrc });
 
   return (
     <Button
@@ -20,7 +22,7 @@ const HoverableArt = ({ src, hoverSrc = null, alt }) => {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <img src={hoverSrc && hovered ? hoverSrc : src} alt={alt} />
+      <CrossfadeImage src={hovered && hoverSrc ? hoverSrc : src} />
     </Button>
   );
 };
